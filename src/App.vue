35e2eeeeb5a6130cabc40168a272eabe5a642996
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-17 14:51:41 +0330

    Adding First Component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<section>
+<h2>My Friends</h2>
+<ul>
+<li></li>
+</ul>
+
+
+</section>
+
+</template>
+<script>
+
+    export default{
+data(){
+return{
+    friends:[
+        {
+            id:'manual',
+            name:'Manual Lorenz',
+            phone:'0123 45678 90',
+            email: 'manual@localhost.com'
+        },
+        {
+            id:'julie',
+            name:'julie Jones',
+            phone:'0987 654421 21',
+            email: 'julie@localhost.com'
+        },
+    ]
+};
+},
+methods:{},
+computed:{},
+    }
+
+
+</script>
