ee1dfdab6a5ae414a0c66878daa002207b5e8fe8
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-19 23:28:52 +0330

    Adding Component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 <section>
 <h2>My Friends</h2>
 <ul>
-<li></li>
+<FriendContact name="Manual Lorez" phone-number="424-8942" email-address="manuel@localhost.com"></FriendContact>
+<FriendContact name="JulieJoens" phone-number="20-249-24" email-address="julie@localhost.com"></FriendContact>
 </ul>
 
 
@@ -10,29 +11,35 @@
 
 </template>
 <script>
+import FriendContact from "./components/FriendContact.vue";
 
     export default{
-data(){
-return{
-    friends:[
-        {
-            id:'manual',
-            name:'Manual Lorenz',
-            phone:'0123 45678 90',
-            email: 'manual@localhost.com'
-        },
-        {
-            id:'julie',
-            name:'julie Jones',
-            phone:'0987 654421 21',
-            email: 'julie@localhost.com'
-        },
-    ]
-};
-},
-methods:{},
-computed:{},
-    }
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manual",
+                    name: "Manual Lorenz",
+                    phone: "0123 45678 90",
+                    email: "manual@localhost.com"
+                },
+                {
+                    id: "julie",
+                    name: "julie Jones",
+                    phone: "0987 654421 21",
+                    email: "julie@localhost.com"
+                },
+            ]
+        };
+    },
+    methods: {},
+    computed: {},
+    components: { FriendContact }
+}
 
 
 </script>
+<style>
+
+
+</style>
