f0c4fd65edfc9966327f2a1b1c96aadd419fe18c
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-21 22:36:18 +0330

    Editing Props

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 <section>
 <h2>My Friends</h2>
 <ul>
-<FriendContact name="Manual Lorez" phone-number="424-8942" email-address="manuel@localhost.com"></FriendContact>
-<FriendContact name="JulieJoens" phone-number="20-249-24" email-address="julie@localhost.com"></FriendContact>
+<FriendContact name="Manual Lorez" phone-number="424-8942" email-address="manuel@localhost.com" is-favorite="1"></FriendContact>
+<FriendContact name="JulieJoens" phone-number="20-249-24" email-address="julie@localhost.com" ></FriendContact>
 </ul>
 
 
