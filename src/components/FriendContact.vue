e2d09e792d1a8f709a2638eade47de337a0411bf
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-25 23:45:21 +0330

    Demo Adding

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{isFavorite  ? '(Favorite)' : ''}}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : ''}}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -13,68 +13,62 @@
         {{ emailAddress }}
       </li>
     </ul>
+    <button @click="$emit('delete', id)">Delete</button>
   </li>
 </template>
 
 <script>
 export default {
- /* props:[
-    'name',
-    'phoneNumber',
-    'emailAddress',
-    'isFavorite'
-  ],
-  */
-  props:{
-    id:{
-      type:String,
-      required:true
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    id: {
+      type: String,
+      required: true
     },
-    name:{
-      type:String,
-      required:true,
+    name: {
+      type: String,
+      required: true,
     },
-    phoneNumber:{
-     type:String,
-      required:true,
+    phoneNumber: {
+      type: String,
+      required: true
     },
-    emailAddress:{
-      type:String,
-      required:true,
+    emailAddress: {
+      type: String,
+      required: true
     },
-    isFavorite:{
-      type:Boolean, required:false,
-        default:false, 
-       // validator:function(value){
-       //   return value === '1' || value === '0'
-       //   }
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
     },
   },
-  emints:['toggle-favorite'],
-  //emits:{
-  //  'toggle-favorite':function(id){
-  //    if(id){
-  //      return true;
-  //    } else {
-  //      console.warn('Id is Missing!!');
-  //      return false;
-  //    }
-  //  }
-//},
+  emits: ['toggle-favorite', 'delete'],
+  // emits: {
+  //   'toggle-favorite': function(id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('Id is missing!');
+  //       return false;
+  //     }
+  //   } 
+  // },
   data() {
     return {
-      detailsAreVisible: false,
-   
+      detailsAreVisible: false
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-      
     },
-    toggleFavorite(){
-      this.$emit('toggle-favorite',this.id);
-    },
-  }
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    }
+  },
 };
 </script>
